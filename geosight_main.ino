a2440788083e60832ca4e51f44ba8c3f10e28919
rumi <39716355+RY786@users.noreply.github.com>
2024-11-08 12:34:06 -0700

    Update geosight_main.ino

--- geosight_main.ino
+++ geosight_main.ino
@@ -1,8 +1,11 @@
 #include <DFRobotDFPlayerMini.h>
 
 DFRobotDFPlayerMini myDFPlayer;
+int button1 = digitalRead(7);
+
 
 void setup() {
+  pinMode(7, INPUT_PULLUP);
   Serial.begin(9600); // Serial monitor
   Serial1.begin(9600); // Serial communication for DFPlayer on Serial1 (TX1/RX1 for Arduino Mega)
 
@@ -13,10 +16,13 @@ void setup() {
 
   Serial.println("DFPlayer Mini online.");
 
-  myDFPlayer.volume(10); // Set volume to 30 (range is 0 to 30)
-  myDFPlayer.play(1); // Play the first file
+  myDFPlayer.volume(20); // Set volume to 30 (range is 0 to 30)
+  if(button1 == HIGH) {
+    myDFPlayer.play(1); // Play the first file
+  }
+
 }
 
 void loop() {
-  // No actions needed in loop for basic playback
+  
 }
