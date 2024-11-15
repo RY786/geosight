526aafa52b86ac1182a42056d5f12fc35b609b58
Robert Gable <71842478+bear217@users.noreply.github.com>
2024-11-14 21:12:22 -0700

    Update geosight_main.ino
    Fixed dogshit code - RY786

--- geosight_main.ino
+++ geosight_main.ino
@@ -1,13 +1,24 @@
 #include <DFRobotDFPlayerMini.h>
 
 DFRobotDFPlayerMini myDFPlayer;
-int button1 = digitalRead(7);
+
+
+const int buttonPin50 = 50; // Pin 50 is connected to the button
+const int buttonPin48 = 48; // Pin 48 is connected to the button
+const int buttonPin49 = 49; // Pin 49 is connected to the button
+
 
 
 void setup() {
-  pinMode(7, INPUT_PULLUP);
-  Serial.begin(9600); // Serial monitor
-  Serial1.begin(9600); // Serial communication for DFPlayer on Serial1 (TX1/RX1 for Arduino Mega)
+  // Start serial communication
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  
+  // Set the button pin as input
+  pinMode(buttonPin50, INPUT_PULLUP);  // No internal pull-up resistor needed (connected to 5V)
+  pinMode(buttonPin49, INPUT_PULLUP);
+  pinMode(buttonPin48, INPUT_PULLUP);
+
 
   if (!myDFPlayer.begin(Serial1)) {
     Serial.println("Unable to begin. Please check connections and SD card.");
@@ -16,13 +27,35 @@ void setup() {
 
   Serial.println("DFPlayer Mini online.");
 
-  myDFPlayer.volume(20); // Set volume to 30 (range is 0 to 30)
-  if(button1 == HIGH) {
-    myDFPlayer.play(1); // Play the first file
-  }
 
+
+  
 }
 
 void loop() {
+  // Read the button state
+  int buttonState50 = digitalRead(buttonPin50);
+  int buttonState48 = digitalRead(buttonPin48);
+  int buttonState49 = digitalRead(buttonPin49);
+
   
+  // Check if the button is pressed
+  myDFPlayer.volume(10); // Set volume to 30 (range is 0 to 30)
+
+  if (buttonState50 == LOW) {  // LOW means the button is pressed
+    Serial.println("Button on pin 50 is pressed!");
+    myDFPlayer.play(1); // Play the first file
+  }
+  if (buttonState49 == LOW) {  // LOW means the button is pressed
+    Serial.println("Button on pin 50 is pressed!");
+    myDFPlayer.play(2); // Play the first file
+  }
+  if (buttonState48 == LOW) {  // LOW means the button is pressed
+    Serial.println("Button on pin 50 is pressed!");
+    myDFPlayer.play(3); // Play the first file
+  }
+  
+  
+  // Small delay to avoid multiple detections
+  delay(50);
 }
